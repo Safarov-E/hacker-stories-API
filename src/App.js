1db4f63c91ad2a541b31c8fad35e66e8252d7869
Safarov <elkhan.safarov10@yandex.com>
2020-09-17 14:20:21 +0500

    Компонуемые компоненты

--- src/App.js
+++ src/App.js
@@ -48,20 +48,40 @@ export default class extends Component {
         <Search 
           value={searchTerm}
           onChange={this.onSearchChange}
-        />
+        >
+          Поиск
+        </Search>
         <Table 
           list={list}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
-        <form>
-          <input 
-            type="text" 
-            value={searchTerm}
-            onChange={this.onSearchChange}  
-          />
-        </form>
-        {list.filter(isSearched(searchTerm)).map((item) => {
+      </div>
+    )
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange, children } = this.props;
+    return (
+      <form>
+        {children} <input 
+          type="text" 
+          value={value}
+          onChange={onChange}  
+        />
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map((item) => {
           return (
             <div key={item.objectID}>
               <span>
@@ -72,7 +92,7 @@ export default class extends Component {
               <span>{item.points}</span>
               <span>
                 <button
-                  onClick={() => this.onDismiss(item.objectID)}
+                  onClick={() => onDismiss(item.objectID)}
                   type="button"
                 >Отбросить</button>
               </span>
