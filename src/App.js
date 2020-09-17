d6c8b36c14bbb0eaf65f376dbc0db6dc6798820f
Safarov <elkhan.safarov10@yandex.com>
2020-09-17 14:10:19 +0500

    Разделение компонентов

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
+
+function isSearched(searchTerm) {
+  return (item)  => {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  }
 }
 
-export default App;
+export default class extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list,
+      searchTerm: ''
+    }
+  }
+  onDismiss = (id) => {
+    const updatedList = this.state.list.filter(item => item.objectID !== id)
+    this.setState({list: updatedList})
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value })
+  }
+  render() {
+    const { searchTerm, list } = this.state;
+    return (
+      <div className="App">
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table 
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+        <form>
+          <input 
+            type="text" 
+            value={searchTerm}
+            onChange={this.onSearchChange}  
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map((item) => {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title} </a>
+              </span>
+              <span>{item.author} </span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >Отбросить</button>
+              </span>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
