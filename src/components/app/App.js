43acc7b0eaab988ad7d3245cfe462a20a08557e6
Safarov <elkhan.safarov10@yandex.com>
2020-09-28 13:46:20 +0500

    Тестирование снимками с помощью Jest

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,15 +4,15 @@ import Table from '../table'
 import Button from '../button'
 import axios from 'axios';
 import './App.css';
-
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_HPP = '100';
-
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP
+} from '../../constants'
 
 export default class extends Component {
   _isMounted = false
--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('Отрисовывает без ошибки', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div)
+  })
+  test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <App />
+    )
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+})
