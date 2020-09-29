0a16108eccb2eb6ede9226bfe7778e51ade1355b
Safarov <elkhan.safarov10@yandex.com>
2020-09-29 14:24:33 +0500

    Ссылка на DOM-элемент

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './search.css';
 
-const Search = ({value, onChange, onSubmit, children}) =>
-  <form onSubmit={onSubmit}>
-    <input 
-      type="text" 
-      value={value}
-      onChange={onChange}  
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
-  
-export default Search
+export default class Search extends Component {
+  componentDidMount() {
+    if(this.input) {
+      this.input.focus()
+    }
+  }
+  render() {
+    const {value, onChange, onSubmit, children} = this.props
+    return (
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          value={value}
+          onChange={onChange}
+          ref={(node) => {this.input = node}} 
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    )
+  }
+}
